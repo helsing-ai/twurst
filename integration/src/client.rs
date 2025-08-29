3a080695d53f8a6db7a830f0e2ab0693c64cb9f3
Mikey Coward <michael.coward@helsing.ai>
2025-08-29 18:30:20 +0100

    revert: Int change attempt

--- integration/src/client.rs
+++ integration/src/client.rs
@@ -121,13 +121,12 @@ impl TryFrom<Data> for TestRequest {
     type Error = Report;
 
     fn try_from(data: Data) -> Result<Self> {
-        let int = Int { value: data.any };
         Ok(Self {
             string: data.string,
             time: Some(data.time.into()),
             nested: Some(data.choice.into()),
             duration: Some(data.duration.try_into()?),
-            any: Some(Any::from_msg(&int)?),
+            any: Some(Any::from_msg(&Int { value: data.any })?),
             value: Some(Value::from(data.value)),
             option: Some(test_request::Option::Right(data.option)),
         })
